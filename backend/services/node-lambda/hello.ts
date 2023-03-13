99acb5d58dee9fe19645e10a0ad8f8e18e1e8e89
James <jim.me.ryan@gmail.com>
2023-03-13 14:11:42 +0000

    Cognito - created authorizer to secure api

--- backend/services/node-lambda/hello.ts
+++ backend/services/node-lambda/hello.ts
@@ -1,18 +1,31 @@
-import { S3 } from 'aws-sdk'
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { S3 } from "aws-sdk";
 
-const s3Client = new S3()
+const s3Client = new S3();
 
-async function handler(event: any, context: any) {
-    console.log('Node Lambda fired!')
-    console.log('updated lambda')
+async function handler(event: APIGatewayProxyEvent, context: any) {
+  console.log("Hello Node Lambda fired!");
 
-    const buckets = await s3Client.listBuckets().promise()
-    console.log('got buckets event: ');
-    console.log(event);
+  if (isAuthorized(event)) {
     return {
-        statusCode: 200,
-        body: 'here are your buckets:' + JSON.stringify(buckets.Buckets)
-    }
-};
+      statusCode: 200,
+      body: JSON.stringify("You are authorized"),
+    };
+  } else {
+    return {
+      statusCode: 401,
+      body: JSON.stringify("You are not authorized"),
+    };
+  }
+}
+
+function isAuthorized(event: APIGatewayProxyEvent): boolean {
+  const groups = event.requestContext.authorizer?.claims["cognito:groups"];
+  if (groups) {
+    return (groups as string).includes("admins");
+  } else {
+    return false;
+  }
+}
 
 export { handler };
