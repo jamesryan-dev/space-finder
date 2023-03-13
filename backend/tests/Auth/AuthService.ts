bab67672b495f27a495ce45f3fb7d4e599ce549d
James <jim.me.ryan@gmail.com>
2023-03-13 21:39:39 +0000

    AWS Cognito Identity Pools

--- backend/tests/Auth/AuthService.ts
+++ backend/tests/Auth/AuthService.ts
@@ -2,6 +2,8 @@ import { Auth, Amplify } from "aws-amplify";
 // import Amplify from "aws-amplify";
 import { config } from "./config";
 import { CognitoUser } from "@aws-amplify/auth";
+import * as AWS from "aws-sdk";
+import { Credentials } from "aws-sdk/lib/credentials";
 
 Amplify.configure({
   Auth: {
@@ -9,6 +11,7 @@ Amplify.configure({
     region: config.REGION,
     userPoolId: config.USER_POOL_ID,
     userPoolWebClientId: config.APP_CLIENT_ID,
+    identityPoolId: config.IDENTITY_POOL_ID,
     authenticationFlowType: "USER_PASSWORD_AUTH",
   },
 });
@@ -22,5 +25,39 @@ export class AuthService {
       console.error("Error signing in:", error);
       throw error;
     }
+  }
+
+  public async getAWSTemporaryCredentials(user: CognitoUser) {
+    // try {
+    const cognitioIdentityPool = `cognito-idp.${config.REGION}.amazonaws.com/${config.USER_POOL_ID}`;
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials(
+      {
+        IdentityPoolId: config.IDENTITY_POOL_ID,
+        Logins: {
+          [cognitioIdentityPool]: user
+            .getSignInUserSession()!
+            .getIdToken()
+            .getJwtToken(),
+        },
+      },
+      { region: config.REGION }
+    );
+    await this.refreshCredentials();
+    // } catch (error) {
+    //   console.error("Error getting temporary credentials:", error);
+    //   throw error;
+    // }
+  }
+
+  private async refreshCredentials(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      (AWS.config.credentials as Credentials).refresh((err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
   }
 }
--- backend/tests/Auth/auth.test.ts
+++ backend/tests/Auth/auth.test.ts
@@ -1,6 +1,34 @@
 import { AuthService } from "./AuthService";
 import { config } from "./config";
+import * as AWS from "aws-sdk";
 
-const authService = new AuthService();
+AWS.config.region = config.REGION;
 
-const user = authService.login(config.TEST_USERNAME, config.TEST_USER_PASSWORD);
+async function getBuckets() {
+  let buckets;
+  try {
+    buckets = await new AWS.S3().listBuckets().promise();
+  } catch (error) {
+    console.error("Error getting buckets:", error);
+    buckets = undefined;
+    // throw error;
+  }
+  return buckets;
+}
+
+async function callStuff() {
+  const authService = new AuthService();
+
+  const user = await authService.login(
+    config.TEST_USERNAME,
+    config.TEST_USER_PASSWORD
+  );
+
+  await authService.getAWSTemporaryCredentials(user);
+
+  const someCreds = AWS.config.credentials;
+  const buckets = await getBuckets();
+  const a = 5;
+}
+
+callStuff();
